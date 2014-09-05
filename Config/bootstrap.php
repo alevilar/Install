da43d7d3768de7cf3e87c118dd050dd98683a69a
Juan Pablo Gonzalez Camera <cameraphp@gmail.com>
2014-09-05 16:45:28 -0400

    Con estos dos cambios solo seria incluir la linea
    CakePlugin::load('Install', array('bootstrap' => true, 'routes' => true));
    
    en el bootstrap.php

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,4 +3,11 @@
 copy(App::pluginPath('Install') . 'Config' . DS . 'Docs' . DS . 'AppController.php', APP . 'Controller' . DS . 'AppController.php');
 copy(App::pluginPath('Install') . 'Config' . DS . 'Docs' . DS . 'AppModel.php', APP . 'Model' . DS . 'AppModel.php');
 
+Configure::write('Risto.installed',
+    file_exists(APP . 'Config' . DS . 'database.php') &&
+    file_exists(APP . 'Config' . DS . 'core.php') &&
+    file_exists(APP . 'Config' . DS . 'resume.php')
+);
+
+
 ?>
--- Config/routes.php
+++ Config/routes.php
@@ -1,7 +1,8 @@
 <?php
 
 $request = Router::getRequest();
-if (strpos($request->url, 'install') === false) {
+
+if (strpos($request->url, 'install') === false&&!Configure::read('Risto.installed')) {
     $url = array('plugin' => 'install', 'controller' => 'install');
 
     Router::redirect('/*', $url, array('status' => 307));
