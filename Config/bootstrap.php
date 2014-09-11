c4d57af894690efadecf5c13cb41e175ea136c18
Juan Pablo Gonzalez Camera <cameraphp@gmail.com>
2014-09-11 08:41:18 -0400

    Omitiendo el uso del copiado inical de ApController y AppModel para arrancar la app Install

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,2 +1,28 @@
 <?php
+if(
+    file_exists(APP . 'Config' . DS . 'database.php')==false
+    ||file_exists(APP . 'Config' . DS . 'core.php')==false
+    ||file_exists(APP . 'Config' . DS . 'risto.php')==false
+    ||file_exists(APP . 'Config' . DS . 'resume.php')==false
+)
+{
 
+    $result = copy(APP . 'Config' . DS . 'database.php.default', APP . 'Config' . DS . 'database.php');
+    if (!$result) {
+        return __d('croogo', 'No se puede copiar el database.php para iniciar la instalación.');
+    }
+// Al vuelo se reemplaza los Ristos Model y Controller para poder arrancar
+App::uses('Controller', 'Controller');
+App::uses('Model', 'Model');
+
+class AppController extends Controller {
+
+}
+
+class AppModel extends Model {
+
+
+}
+
+
+}
