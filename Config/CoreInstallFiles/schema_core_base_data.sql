148bdbf8598281ab7476275f84fc6135297d8c9a
Ale Vilar <alevilar@gmail.com>
2015-09-17 19:40:35 -0300

    borrado de sql que ya no sirven mas

--- Config/CoreInstallFiles/schema_core_base_data.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO `roles` (`id`, `name`, `machin_name`, `created`, `modified`) VALUES
-(1, 'Superadmin', 'superadmin', NULL, NULL);
-
--- Config/CoreInstallFiles/schema_core_drop_tables.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `users`, `sites`, `sites_users`;
